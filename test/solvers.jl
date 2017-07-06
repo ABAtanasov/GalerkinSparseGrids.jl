10425c6b5682bb30449d7d9df1480874e70114aa
AlexAtanasov <alexander.atanasov@yale.edu>
2017-07-05 21:43:16 -0700

    start of v0.6 compatibility changes

--- test/solvers.jl
+++ test/solvers.jl
@@ -22,7 +22,7 @@ v0 = x->2*pi*cos(2*pi*x[1])
 pos_soln = wave_evolve_1D(k, level, f0, v0, 0, 1; base="pos", order="45")
 energy_soln = energy_func_1D(k, level, pos_soln; base="pos")
 for i in 1:length(energy_soln[2])
-    @test abs(sqrt(energy_soln[2][i])-2*pi)<=1.0e-7
+    @test sqrt(energy_soln[2][i]) ≈ 2*pi atol=1.0e-7
 end
 
 println("Test Passed.")
@@ -34,7 +34,13 @@ print("Testing wave equation solver 1-D hierarchical DG basis... ")
 hier_soln  = wave_evolve_1D(k, level, f0, v0, 0, 1; base="hier", order="45")
 energy_soln = energy_func_1D(k, level, hier_soln; base="hier")
 for i in 1:length(energy_soln[2])
-    @test abs(sqrt(energy_soln[2][i])-2*pi)<=1.0e-7
+    @test sqrt(energy_soln[2][i]) ≈ 2*pi atol=1.0e-7
+end
+
+hier_soln  = wave_evolve(1, k, level, f0, v0, 0, 1; order="78")
+energy_soln = energy_func(1, k, level, hier_soln; order="78")
+for i in 1:length(energy_soln[2])
+    @test sqrt(energy_soln[2][i]) ≈ 2*pi atol=1.0e-7
 end
 
 println("Test Passed.")
@@ -50,27 +56,27 @@ k_used = 3
 n_used = 5
 f0 = x->sin(2*pi*x[1])*sin(2*pi*x[2])
 v0 = x->0
-sparse_soln = wave_evolve(D, k_used, n_used, f0, v0, 0,1; order="78", scheme="sparse");
+sparse_soln = wave_evolve(D, k_used, n_used, f0, v0, 0, 1; order="78", scheme="sparse");
 senergy = energy_func(D, k_used, n_used, sparse_soln)
 @test senergy[2][1]-senergy[2][end] > 0
 @test abs(senergy[2][1]-senergy[2][end]) < 1.0e-8
 for i in 1:length(senergy[1])
-    @test abs(sqrt(senergy[2][i])-sqrt(2)*pi) <= 1.0e-4
+    @test sqrt(senergy[2][i]) ≈ sqrt(2)*pi atol=1.0e-4
 end
 
 sparse_soln = wave_evolve(D, k_used, n_used, f0, v0, 0,1; order="45", scheme="sparse");
 senergy = energy_func(D, k_used, n_used, sparse_soln)
-@test senergy[2][1]-senergy[2][end]>0
-@test abs(senergy[2][1]-senergy[2][end])<1.0e-8
+@test senergy[2][1] > senergy[2][end]
+@test senergy[2][1] ≈ senergy[2][end] atol=1.0e-8
 for i in 1:length(senergy[1])
-    @test abs(sqrt(senergy[2][i])-sqrt(2)*pi)<=1.0e-4
+    @test sqrt(senergy[2][i]) ≈ sqrt(2)*pi atol=1.0e-4
 end
 
 println("Test Passed.")
 
 print("Testing traveling wave example in 2-D... ")
 
-m = [1,2]
+m = [1, 2]
 truesoln = x -> cos(2*pi*(vecdot(m,x) - sqrt(vecdot(m,m))*0.54))
 
 D = length(m)
